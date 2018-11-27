95054fcd27882224299ea8c12eaa8b08f6a9c088
hanabusa <sigre7944@gmail.com>
2018-11-27 20:49:27 +0200

    send and receive OK

--- robot1/robot1.ino
+++ robot1/robot1.ino
@@ -1,16 +1,37 @@
-#include <IRLibAll.h>
+/*
+ * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+ * An IR detector/demodulator must be connected to the input RECV_PIN.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
 
-IRsendSony mySender;
+#include <IRremote.h>
 
-int button_pin = 13; //set D10 as input signal pin
+int RECV_PIN = 10;
 
-void setup(){
+IRrecv irrecv(RECV_PIN);
+
+IRsend irsend;
+
+decode_results results;
+
+void setup()
+{
   Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
 }
 
-void loop(){
-  if (Serial.read() != -1)
-  {
-    mySender.send(0xa8bca, 20);
-    }
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.print("Protocol: ");
+    Serial.println(results.decode_type, DEC);
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
 }
